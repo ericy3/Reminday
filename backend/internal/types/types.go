3eacfeec78ee36d9424de863cce986f309cde167
Eric Yang <ericy3ng@gmail.com>
2025-02-26 22:07:23 -0800

    JWT token creation function and event database interface setup start

--- backend/internal/types/types.go
+++ backend/internal/types/types.go
@@ -1,6 +1,12 @@
 package types
 
 import (
+	"log"
+	"os"
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/joho/godotenv"
 	"golang.org/x/crypto/bcrypt"
 )
 
@@ -20,6 +26,10 @@ type UserStore interface {
 	GetUser(username string) (User, error)
 }
 
+type EventStore interface {
+	AddEvent(username string, event string)
+}
+
 func NewUser(registerUser RegisterUser) (User, error) {
 	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(registerUser.Password), 10)
 	if err != nil {
@@ -34,4 +44,27 @@ func NewUser(registerUser RegisterUser) (User, error) {
 func ValidateUser(hashedPassword string, plainTextPassword string) bool {
 	err := bcrypt.CompareHashAndPassword([]byte(hashedPassword), []byte(plainTextPassword))
 	return err == nil
+}
+
+func CreateToken(user User) string {
+	now := time.Now()
+	validUntil := now.Add(time.Hour * 1).Unix()
+
+	claims := jwt.MapClaims{
+		"user":    user.Username,
+		"expires": validUntil,
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims, nil)
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	secret := os.Getenv("SECRET")
+	tokenString, err := token.SignedString([]byte(secret))
+	if err != nil {
+		return ""
+	}
+	return tokenString
 }
